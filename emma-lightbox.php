538f933efa752a174c053cb69ef9dde33dcb65d4
Eric Michel <eric.r.michel@gmail.com>
2021-10-26 13:12:04 -0400

    updated lightgallery and modified custom code for greater configurability in child theme

--- emma-lightbox.php
+++ emma-lightbox.php
@@ -14,17 +14,26 @@ Text Domain: emma_lightbox
  * Register and enqueue frontend scripts and styles
  */
 function emma_lightbox_enqueue_frontend() {
-  $lightbox_library_js = plugin_dir_url( __FILE__ ) . 'vendor/lightgallery/js/lightgallery.min.js';
-  $lightbox_library_css = plugin_dir_url( __FILE__ ) . 'vendor/lightgallery/css/lightgallery.min.css';
-  $lightbox_library_version = '1.9.0';
+  $lightbox_library_js = plugin_dir_url( __FILE__ ) . 'vendor/lightgallery/lightgallery.min.js';
+  $lightbox_library_css = plugin_dir_url( __FILE__ ) . 'vendor/lightgallery/css/lightgallery-bundle.min.css';
+  $lightbox_library_version = '2.2.1';
+
+  $lightbox_default_plugins = array(
+    'lg-thumbnail',
+  );
+  $lightbox_plugins = apply_filters( 'lightbox_plugins', $lightbox_default_plugins );
   
-  wp_enqueue_script( 'lightbox-library', $lightbox_library_js, [], $lightbox_library_version, true );
   wp_enqueue_style( 'lightbox-library', $lightbox_library_css, [], $lightbox_library_version );
+  wp_enqueue_script( 'lightbox-library', $lightbox_library_js, [], $lightbox_library_version, true );
+  foreach( $lightbox_plugins as $plugin ) {
+    $plugin_js = plugin_dir_url( __FILE__ ) . 'vendor/lightgallery/plugins/thumbnail/' . $plugin . '.min.js';
+    wp_enqueue_script( $plugin, $plugin_js, ['lightbox-library'], $lightbox_library_version, true );
+  }
   
   $lightbox_frontend_js = plugin_dir_url( __FILE__ ) . 'js/lightbox-frontend.js';
   $lightbox_frontend_version = '1.0.0';
 
-  wp_enqueue_script( 'lightbox-frontend', $lightbox_frontend_js, ['lightbox-library'], $lightbox_frontend_version, true );
+  wp_enqueue_script( 'lightbox-frontend', $lightbox_frontend_js, ['lightbox-library', 'emma-scripts'], $lightbox_frontend_version, true );
 
 }
 add_action( 'wp_enqueue_scripts', 'emma_lightbox_enqueue_frontend' );
--- js/lightbox-frontend.js
+++ js/lightbox-frontend.js
@@ -1,15 +1,29 @@
 ( function() {
+  const defaultLightGallerySettings = {
+    licenseKey: 'DD622256-1B89401A-8FC0F735-98E8EA6F',
+    selector: 'figure a',
+    download: false,
+    plugins: [lgThumbnail],
+    thumbnail: true,
+    zoomFromOrigin: false,
+  };
+
+  if( typeof window.lightGallerySettings === 'undefined' ) {
+    var lightGallerySettings = defaultLightGallerySettings;
+  } else {
+    var lightGallerySettings = window.lightGallerySettings;
+  }
+
   document.querySelectorAll( ".lightgallery" ).forEach( gallery => {
     gallery.querySelectorAll( 'figure a' ).forEach( anchor => {
       var img = anchor.querySelector( 'img' );
-      var srcset = img.srcset;
-      var sizes = img.sizes;
-      anchor.dataset.srcset = srcset;
-      anchor.dataset.sizes = sizes;
+      if( anchor.href == img.src ) {
+        var srcset = img.srcset;
+        var sizes = img.sizes;
+        anchor.dataset.srcset = srcset;
+        anchor.dataset.sizes = sizes;
+      }
     } );
-    lightGallery( gallery, {
-      selector: 'figure a',
-      download: false,
-    } );
+    lightGallery( gallery, lightGallerySettings );
   } );
 } )();
